29e43a85c6068879d66edf8df9036b2e53f83769
Familia Quintanilla <Familia Quintanilla@192.168.1.15>
2018-05-13 19:49:42 -0600

    hermite falta 2da derivada

--- src/analisis04/Analisis04.java
+++ src/analisis04/Analisis04.java
@@ -7,13 +7,13 @@ import metodos.hermite;
 public class Analisis04 {
 
     public static void main(String[] args) {
-        Newton l = new Newton();
-        double b[]={0,2,9,4,5};
-        double a[]={0,1,2,3,4};
-        l.Newton(a, b);
-        
-        Lagrange s = new Lagrange();
-        s.Lagrange(a, b, 2);
+//        Newton l = new Newton();
+//        double b[]={0,2,9,4,5};
+//        double a[]={0,1,2,3,4};
+//        l.Newton(a, b);
+//        
+//        Lagrange s = new Lagrange();
+//        s.Lagrange(a, b, 2);
         
         hermite h= new hermite();
         double x[]={1, 1, 2, 2};
--- src/metodos/hermite.java
+++ src/metodos/hermite.java
@@ -9,6 +9,58 @@ public class hermite {
 
     JEP jp = new JEP();
 
+    public void Hermite(double x[], double y[], String funcion) {
+        String pol = "", sal = "";
+        double denominador = 0, numerador = 0, val=0;
+        //lenando x, y
+        Object matriz[][] = new Object[x.length][x.length + 1];
+        for (int i = 0; i < x.length; i++) {
+            matriz[i][0] = x[i];
+            matriz[i][1] = y[i];
+        }
+        //llendando matriz
+        int cn = 2;
+        for (int i = 1; i < x.length + 1; i++, cn++) {
+            for (int j = 1; j < x.length; j++) {
+                if (i == 1) {
+                    numerador=(((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1]));
+                    denominador=(Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]));
+                    if(numerador ==0 && denominador==0){
+                         sal = Derivar(funcion);
+                        val=val(sal,x[j]);
+                        matriz[j][cn]=val;
+                    }else{
+                    matriz[j][cn] = numerador / denominador;
+                    }
+                } else {
+                    int o = j + i - 1;
+                    if (o < x.length) {
+                        numerador=(((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1]));
+                        denominador=(Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - i]));
+                        if(numerador ==0 && denominador==0){
+                         sal = Derivar(funcion);
+                        val=val(sal,x[j]);
+                        matriz[j][cn]=val;
+                    }else{
+                        matriz[o][cn] = numerador / denominador;
+                        }
+                    }
+                }
+            }
+        }
+
+        //imprimiendo matriz
+        for (int s = 0; s < x.length; s++) {
+            for (int j = 0; j < x.length + 1; j++) {
+                if (matriz[s][j] != null) {
+                    System.out.print(matriz[s][j] + " |\t");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+
     public hermite() {
         jp.addStandardConstants();
         jp.addStandardFunctions();
@@ -23,55 +75,6 @@ public class hermite {
             System.out.println("Ecuancion no entendible:\n" + jp.getErrorInfo());
         }
         return jp.getValue();
-    }
-
-    public void Hermite(double x[], double y[], String funcion) {
-        String pol = "", sal = "";
-        double denominador = 0, numerador = 0;
-        //lenando x, y
-        Object matriz[][] = new Object[x.length][x.length + 1];
-        for (int i = 0; i < x.length; i++) {
-            matriz[i][0] = x[i];
-            matriz[i][1] = y[i];
-        }
-        //llendando matriz
-        int col = 2, cont = 0;
-        for (int i = 1; i < x.length + 1; i++, col++) {
-            for (int j = 1; j < x.length; j++) {
-                if (i == 1) {
-                    numerador = ((double) matriz[j][col - 1] - (double) matriz[j - 1][col - 1]);
-                    denominador = Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]);
-//                    if (numerador == 0 && denominador == 0) {
-//                        sal = Derivar(funcion);
-//                        matriz[j][col] = val(sal, (double) matriz[j][col]);
-//                    } else {
-//                        matriz[j][col] = numerador / denominador;
-//                    }
-                } else {
-                    int o = j + i - 1;
-                    if (o < x.length) {
-                        numerador = ((double) matriz[o][col - 1] - (double) matriz[o - 1][col - 1]);
-                        denominador = (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - i]));
-//                        if (numerador == 0 && denominador == 0) {
-//                            sal = Derivar(funcion);
-//                            matriz[j][col] = val(sal, (double) matriz[j][col]);
-//                        } else {
-//                            matriz[j][col] = numerador / denominador;
-//                        }
-                    }
-                }
-            }
-        }
-        //imprimiendo matriz
-        for (int s = 0; s < x.length; s++) {
-            for (int j = 0; j < x.length + 1; j++) {
-                if (matriz[s][j] != null) {
-                    System.out.print(matriz[s][j] + " |\t");
-                }
-            }
-            System.out.println("");
-        }
-
     }
 
     public String Derivar(String fun) {
