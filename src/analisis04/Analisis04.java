f366996051625ab0eb2a84821c5babbacf316c79
robal <robal@AREVALO>
2018-05-19 23:17:08 -0600

    falta derivar hermite

--- src/analisis04/Analisis04.java
+++ src/analisis04/Analisis04.java
@@ -7,22 +7,22 @@ import metodos.hermite;
 public class Analisis04 {
 
     public static void main(String[] args) throws Exception {
-        Newton l = new Newton();
-        double b[] = {1, 0.5, 2, 2.55, 4};
-        double a[] = {0.4, 2.5, 4.3, 5, 6};
-        l.Newton(a, b);
+//        Newton l = new Newton();
+        double b[] = {125350,133420,117183,120323,145311};
+        double a[] = {1910,1930,1950,1960,1980};
+//        l.Newton(a, b);
 
         Lagrange s = new Lagrange();
-        String p[] = s.Lagrange(a, b, 5.2);
+        String p[] = s.Lagrange(a, b, 1964);
         for (int i = 0; i < p.length; i++) {
             System.out.println("p:" + p[i]);
         }
-
-        hermite h = new hermite();
-        double x[] = {1, 1, 2, 2};
-        double y[] = {0, 0, 0.69314718, 0.69314718};
-        String funcion = "ln(x)";
-        h.Hermite(x, y, funcion);
+//
+//        hermite h = new hermite();
+//        double x[] = {1, 1, 2, 2,};
+//        double y[] = {0, 0, 0.69314718, 0.69314718};
+//        String funcion = "ln(x)";
+//        h.Hermite(x, y, funcion);
     }
 
 }
