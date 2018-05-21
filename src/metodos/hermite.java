bfbfd39a590b49b1e2f8830c7fd9f7f6273f8b1c
robal <robal@platform.wondershare.com>
2018-05-21 09:17:38 -0600

    casi terminado hermite

--- src/metodos/hermite.java
+++ src/metodos/hermite.java
@@ -104,46 +104,46 @@ public class hermite {
             System.out.println("der: "+Derivada[i]);
         }
         //lenando x, y
-//        Object matriz[][] = new Object[x.length][x.length + 1];
-//        for (int i = 0; i < x.length; i++) {
-//            matriz[i][0] = x[i];
-//            matriz[i][1] = y[i];
-//        }
-//        //llendando matriz
-//        int cn = 2;
-//        if (!funcion.equals("")) {
-//            for (int i = 1; i < x.length + 1; i++, cn++) {
-//            for (int j = 1; j < x.length; j++) {
-//                if (i == 1) {
-//                    if ((((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1]))==0 && (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]))==0) {
-//                        matriz[j][cn]=Derivada[0];
-//                        System.out.println("hola");
-//                    }else{
-//                        matriz[j][cn] = (((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1])) / (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]));                        
-//                    }
-//                } else {
-//                    int o = j + i - 1;
-//                    if (o < x.length) {
-//                         if ((((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1]))==0 && (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - 1]))==0) {
-//                             matriz[j][cn]=Derivada[0];
-//                             System.out.println("hola");
-//                        }else{
-//                             matriz[o][cn] = (((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1])) / (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - i]));
-//                         }
-//                    }
-//                }
-//            }
-//        }
-//        }
-//        //imprimiendo matriz
-//        for (int s = 0; s < x.length; s++) {
-//            for (int j = 0; j < x.length + 1; j++) {
-//                if (matriz[s][j] != null) {
-//                    System.out.print(matriz[s][j] + " |\t");
-//                }
-//            }
-//            System.out.println("");
-//        }
+        Object matriz[][] = new Object[x.length][x.length + 1];
+        for (int i = 0; i < x.length; i++) {
+            matriz[i][0] = x[i];
+            matriz[i][1] = y[i];
+        }
+        //llendando matriz
+        int cn = 2;
+        if (!funcion.equals("")) {
+            for (int i = 1; i < x.length + 1; i++, cn++) {
+            for (int j = 1; j < x.length; j++) {
+                if (i == 1) {
+                    if ((((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1]))==0 && (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]))==0) {
+                        matriz[j][cn]=Derivada[0];
+                        System.out.println("hola");
+                    }else{
+                        matriz[j][cn] = (((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1])) / (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]));                        
+                    }
+                } else {
+                    int o = j + i - 1;
+                    if (o < x.length) {
+                         if ((((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1]))==0 && (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - 1]))==0) {
+                             matriz[j][cn]=Derivada[0];
+                             System.out.println("hola");
+                        }else{
+                             matriz[o][cn] = (((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1])) / (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - i]));
+                         }
+                    }
+                }
+            }
+        }
+        }
+        //imprimiendo matriz
+        for (int s = 0; s < x.length; s++) {
+            for (int j = 0; j < x.length + 1; j++) {
+                if (matriz[s][j] != null) {
+                    System.out.print(matriz[s][j] + " |\t");
+                }
+            }
+            System.out.println("");
+        }
 
     }
 }
