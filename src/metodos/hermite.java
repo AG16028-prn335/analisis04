64cd4cfbade398178086a60ee9b3f44faa040d0f
robal <robal@AREVALO>
2018-05-21 17:58:42 -0600

    hermite

--- src/metodos/hermite.java
+++ src/metodos/hermite.java
@@ -8,151 +8,78 @@ public class hermite {
     }
 
     public String[] Hermite(double x[], double y[], double x1[], double y1[], double n) {
-//        String pol = "", sal = "";
-//        double nx[], Derivada[] = {1};
-//        int co = 0, con = 1;
-//        for (int i = 1; i < x.length; i++) {
-//            if (x[i] != x[i - 1]) {
-//                co++;
-//            }
-//        }
-//        nx = new double[2 * (co + 1)];
-//        co=2 * (co + 1)-1;
-//        for (int i = 1; i < x.length; i++) {
-//            con=1;
-//            if (i != 1) {
-//                if (x[i] != x[i - 1]) {
-//                        nx[co] = x[i];
-//                        co--;
-//                } else {
-//                    for (int j = i; j <x.length; j++) {
-//                        if (x[j]==x[j-1]) {
-//                            con++;
-//                        }else{
-//                            break;
-//                        }
-//                    }
-//                    nx[co]=con;
-//                    co--;
-//                    i+=con-2;
-//                }
-//            } else {
-//                nx[co] = x[i - 1];
-//                    co--;
-//                    for (int j = 1; j <x.length; j++) {
-//                        if (x[j]==x[j-1]) {
-//                            con++;
-//                        }else{
-//                            break;
-//                        }
-//                    }
-//                    nx[co]=con;
-//                    co--;
-//                    i+=con-2;
-//            }
-//        }
-//        con=0;
-//        for (int i = 0; i <nx.length; i++) {
-//            if (i%2==0) {
-//                con+=nx[i]-1;
-//            }
-//        }
-//        Derivada= new double[con+(nx.length)/2];
-//        //con este metodo se ordena todo lo de los numeros repetidos
-//        co=0;
-//        con=0;
-//        for (int i = 0; i <(nx.length)/2; i++) {
-//            double burbuja=nx[i];
-//            nx[i]=nx[nx.length-1-i];
-//            nx[nx.length-1-i]=burbuja;
-//        }
-//        for (int i = 0; i <nx.length; i++) {
-//            if (i%2==0) {
-//                Derivada[co]=nx[i];
-//                co+=(int) nx[i+1];
-//            }
-//        }
-//        con=1;
-//        if (!funcion.isEmpty()) {
-//             
-//            for (int i = 0; i <nx.length; i++) {
-//                pol=funcion;
-//                if (i%2!=0) {
-//                    co=(int)nx[i];
-//               while(co>1){
-//                   if (Derivada[con]==0) {
-//                       pol=p.Derivar(pol);
-//                       Derivada[con]=p.Ecuacion(pol, nx[i-1]);
-//                       con++;
-//                   }else{
-//                       break;
-//                   }
-//               co--;    
-//               }
-//               con++;
-//                }
-//            }
-//        }else{
-//            for (int i = 0; i <Derivada.length; i++) {
-//                if (Derivada[i]==0) {
-//                    Derivada[i]=con;
-//                    //solo puse con para q no de error
-//                }
-//            }
-//        }
-//        for (int i = 0; i <Derivada.length; i++) {
-//            System.out.println("der: "+Derivada[i]);
-//        }
+        String pol = "", sal = "";
+        int co = 0, con = 1;
+//        
 //        //lenando x, y
-//        Object matriz[][] = new Object[x.length][x.length + 1];
-//        for (int i = 0; i < x.length; i++) {
-//            matriz[i][0] = x[i];
-//            matriz[i][1] = y[i];
-//        }
+        Object matriz[][] = new Object[x.length][x.length + 1];
+        for (int i = 0; i < x.length; i++) {
+            matriz[i][0] = x[i];
+            matriz[i][1] = y[i];
+        }
 //        //llendando matriz
-//        int cn = 2;
-//        if (!funcion.equals("")) {
-//            for (int i = 1; i < x.length + 1; i++, cn++) {
-//            for (int j = 1; j < x.length; j++) {
-//                if (i == 1) {
-//                    if ((((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1]))==0 && (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]))==0) {
-//                        matriz[j][cn]=Derivada[0];
-//                        System.out.println("hola");
-//                    }else{
-//                        matriz[j][cn] = (((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1])) / (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]));                        
-//                    }
-//                } else {
-//                    int o = j + i - 1;
-//                    if (o < x.length) {
-//                         if ((((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1]))==0 && (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - 1]))==0) {
-//                             matriz[j][cn]=Derivada[0];
-//                             System.out.println("hola");
-//                        }else{
-//                             matriz[o][cn] = (((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1])) / (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - i]));
-//                         }
-//                    }
-//                }
-//            }
-//        }
-//        }
-//        //imprimiendo matriz
-//        for (int s = 0; s < x.length; s++) {
-//            for (int j = 0; j < x.length + 1; j++) {
-//                if (matriz[s][j] != null) {
-//                    System.out.print(matriz[s][j] + " |\t");
-//                }
-//            }
-//            System.out.println("");
-//        }
-String[] p ={"holaa"};
-return p; 
+        int cn = 2;
+            for (int i = 1; i < x.length + 1; i++, cn++) {
+            for (int j = 1; j < x.length; j++) {
+                if (i == 1) {
+                    if ((((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1]))==0 && (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]))==0) {
+                        matriz[j][cn]=x1[i];
+                    }else{
+                        matriz[j][cn] = (((double) matriz[j][cn - 1] - (double) matriz[j - 1][cn - 1])) / (Double.parseDouble("" + x[j]) - Double.parseDouble("" + x[j - 1]));                        
+                    }
+                } else {
+                    int o = j + i - 1;
+                    if (o < x.length) {
+                         if ((((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1]))==0 && (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - 1]))==0) {
+                             matriz[j][cn]=x1[o];
+                        }else{
+                             matriz[o][cn] = (((double) matriz[o][cn - 1] - (double) matriz[o - 1][cn - 1])) / (Double.parseDouble("" + x[o]) - Double.parseDouble("" + x[o - i]));
+                         }
+                    }
+                }
+            }
+        }
+        //imprimiendo matriz
+        for (int s = 0; s < x.length; s++) {
+            for (int j = 0; j < x.length + 1; j++) {
+                if (matriz[s][j] != null) {
+                    System.out.print(matriz[s][j] + " |\t");
+                }
+            }
+            System.out.println("");
+        }
+        String fun="",fac="";
+        double bn []= new double[x.length+1];
+        for (int i = 0; i < x.length; i++) {
+                bn[i]=(double)matriz[i][i+1];
+        }
+        String factor = "(x-$)";
+        for (int i = 0; i < x.length; i++) {
+                if (bn[i] < 0) {
+                    fun += " " + bn[i];
+                } else {
+                    fun += " +" + bn[i];
+                }
+
+                for (int j = 0; j < i; j++) {
+                    if (bn[i] != 0) {
+                        fun += factor.replace("$", String.valueOf(x[j]));
+                    }
+                }
+            }
+        
+String[] p1 ={p.poli(fun),""+p.Ecuacion(p.poli(fun), n),ErrorHermite(x, bn[bn.length-1], n)};
+        for (int i = 0; i <p1.length; i++) {
+            System.out.println(""+p1[i]);
+        }
+return p1; 
     }
-    /*public String ErrorHermite(double a[], double ult, double v) {
+    public String ErrorHermite(double a[], double ult, double v) {
         String s = "", er;
         for (int i = 0; i < a.length; i++) {
             s += "(x -" + a[i] + ")^2";
         }
-        er = "(" + ult + ")*" + s;
+        er = "(" + ult + ")" + s;
         return p.Ecuacion(er, v) + "";
-    }*/
+    }
 }
